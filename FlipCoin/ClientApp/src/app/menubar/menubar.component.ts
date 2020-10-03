61354ad2312ca88054ada62c47928446543e87df
Will Frank <willfrank98@gmail.com>
2020-10-03 15:05:51 -0600

    Add project files.

---
+++ FlipCoin/ClientApp/src/app/menubar/menubar.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'menu-bar',
+  templateUrl: './menubar.component.html'
+})
+export class MenubarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ FlipCoin/Models/ApplicationUser.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FlipCoin.Models
+{
+	public class ApplicationUser : IdentityUser
+	{
+	}
+}
